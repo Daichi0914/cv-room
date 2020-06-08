7aa5924be621231487959412e9d9ecf77284fe03
iida nozomi <nozomi@mbp.local>
2020-06-08 10:56:20 +0900

    Home.js

---
+++ src/conponents/home/Home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h3><span><i className="users icon"></i></span>チームリスト</h3>
+      <div className="ui card">
+        <div className="content">
+          <div className="header">Project Timeline</div>
+        </div>
+        <div className="content">
+          <h4 className="ui sub header">Activity</h4>
+          <div className="ui small feed">
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Elliot Fu added Jenny Hess to the project
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Stevie Feliciano was added as an Administrator
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Helen Troy added two pictures
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui card">
+        <div className="content">
+          <div className="header">Project Timeline</div>
+        </div>
+        <div className="content">
+          <h4 className="ui sub header">Activity</h4>
+          <div className="ui small feed">
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Elliot Fu added Jenny Hess to the project
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Stevie Feliciano was added as an Administrator
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Helen Troy added two pictures
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <i className="plus circle icon"></i>
+    </div>
+      
+  )
+}
